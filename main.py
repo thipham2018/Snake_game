ef7c4d29d8ee3c9ba217104a44b8ada9da44af0d
thipham2018 <trangphamtmp@gmail.com>
2024-01-05 10:40:17 -0500

    commit 1

---
+++ main.py
@@ -0,0 +1,15 @@
+# Import packages
+import turtle
+import random
+import time
+
+# creating screen
+screen = turtle.Screen()
+screen.title("SNACK GAME")
+screen.setup(width=700, height=700)
+screen.tracer(0)
+screen.bgcolor("#1d1d1d")
+
+# creating border
+turtle.speed(5)
+
