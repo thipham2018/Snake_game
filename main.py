0de82a8b6d329f1294c39a973db44bd4b55cb37e
thipham2018 <trangphamtmp@gmail.com>
2024-01-05 10:57:46 -0500

    add scoring

--- main.py
+++ main.py
@@ -12,4 +12,52 @@ screen.bgcolor("#1d1d1d")
 
 # creating border
 turtle.speed(5)
+turtle.pensize(4)
+turtle.penup()
+turtle.goto(-310, 250)
+turtle.pendown()
+turtle.color("red")
+turtle.forward(600)
+turtle.right(90)
+turtle.forward(600)
+turtle.right(90)
+turtle.forward(600)
+turtle.right(90)
+turtle.forward(500)
+turtle.penup()
+turtle.hideturtle()
 
+# Score
+score = 0
+delay = 0.1
+
+# Snake
+snake = turtle.Turtle()
+snake.speed()
+snake.shape("square")
+snake.color("green")
+snake.penup()
+snake.goto(0, 0)
+snake.direction = 'stop'
+
+# Food
+fruit = turtle.Turtle()
+fruit.speed(0)
+fruit.shape("square")
+fruit.color("white")
+fruit.penup()
+fruit.goto(30, 30)
+
+old_fruit = []
+
+# scoring
+scoring = turtle.Turtle()
+scoring.speed(0)
+scoring.color("white")
+scoring.penup()
+scoring.hideturtle()
+scoring.goto(0, 300)
+scoring.write("Score: ", align="center", font=("Courier", 24, "bold"))
+
+# define how to move
+#def snake_go_up():
