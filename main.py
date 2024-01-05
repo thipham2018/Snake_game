da005657d1579dba6a7adc65a561935a33777eb2
thipham2018 <trangphamtmp@gmail.com>
2024-01-05 13:18:25 -0500

    add random to snake

--- main.py
+++ main.py
@@ -116,6 +116,30 @@ while True:
         score += 1
         scoring.write("Score: {}".format(score), align="center", font=("Courier", 24, "bold"))
         delay -= 0.001
+    # Creating new foods
+    new_fruit = turtle.Turtle()
+    new_fruit.speed(0)
+    new_fruit.shape("square")
+    new_fruit.color("red")
+    new_fruit.penup()
+    old_fruit.append(new_fruit)
+
+    # Adding ball to snake
+    for index in range(len(old_fruit) -1, 0, -1):
+        a = old_fruit[index -1].xcor()
+        b = old_fruit[index -1].ycor()
+
+        old_fruit[index].goto(a, b)
+    if len(old_fruit) > 0:
+        a = snake.xcor()
+        b = snake.ycor()
+        old_fruit[0].goto(a, b)
+   # snake_move()
+
+    # Snake & border situation
+
+
+
 
 
 
