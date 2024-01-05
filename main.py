ec9ca8370741aa43665aa7b48ea9d104f8548334
thipham2018 <trangphamtmp@gmail.com>
2024-01-05 13:09:32 -0500

    add random to snake

--- main.py
+++ main.py
@@ -111,6 +111,12 @@ while True:
     if snake.distance(fruit) < 20:
         x = random.randint(-290, 270)
         y = random.randint(-240, 240)
+        fruit.goto(x, y)
+        scoring.clear()
+        score += 1
+        scoring.write("Score: {}".format(score), align="center", font=("Courier", 24, "bold"))
+        delay -= 0.001
+
 
 
 
